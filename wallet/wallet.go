12794c5dc61a54f8310e458d5ab903f032e513e9
BIUBIULUOXX <45119449+BIUBIULUOXX@users.noreply.github.com>
2020-12-07 19:34:58 +0800

    *zhi zheng

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,15 +2,24 @@ package main
 
 import "fmt"
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w Wallet) Deposit(amount int) {
-	fmt.Println("address of balance in Deposit is", &w.balance)
+type Stringer interface {
+	String() string
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
