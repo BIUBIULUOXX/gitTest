a773e7b182f19dd90bad5ae637b8ece42594e63e
BIUBIULUOXX <45119449+BIUBIULUOXX@users.noreply.github.com>
2020-12-23 22:16:19 +0800

    finish

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -6,7 +6,6 @@ import (
 
 func TestWallet(t *testing.T) {
 
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -16,9 +15,10 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 		
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t,err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -26,7 +26,7 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, Bitcoin(20))
-		assertError(t, err, "cannot withdraw, insufficient funds")
+		assertError(t, err, InsufficientFundsError)
 
 	})
 
